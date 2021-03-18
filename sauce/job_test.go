b687c014779348829ece42efea8a6061fac1b3dd
Sully Suleiman <ssuleiman955@gmail.com>
2021-03-18 13:34:11 -0500

    implemented query param job test

--- sauce/job_test.go
+++ sauce/job_test.go
@@ -19,6 +19,19 @@ func MockPositiveJobResponse(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&resp)
 }
 
+func MockPositiveJobResponseQueryParams(w http.ResponseWriter, r *http.Request) {
+	resp := Jobs{
+		Job{
+			Status: "Completed",
+		},
+		Job{
+			Status: "Failed",
+		},
+	}
+
+	json.NewEncoder(w).Encode(&resp)
+}
+
 func MockNegativeJobResponse(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	w.Write([]byte("Not found"))
@@ -32,11 +45,32 @@ func TestGetJob(t *testing.T) {
 	//ensure we close connection
 	defer s.Close()
 
-	c := NewClient(os.Getenv("SAUCE_KEY"), "ssuleiman", s.URL)
-	val, err := c.GetJobs()
+	c := NewClient(os.Getenv("SAUCE_KEY"), "test", s.URL)
+	val, err := c.GetJobs(nil)
 	if err == nil {
 		fmt.Println(val)
 		t.Log("Test passed, got jobs for valid user")
+	}
+}
+
+func TestGetJobQueryParams(t *testing.T) {
+	//server a fake job back
+	s := httptest.NewServer(http.HandlerFunc(MockPositiveJobResponseQueryParams))
+
+	//ensure we close connection
+	defer s.Close()
+
+	c := NewClient(os.Getenv("SAUCE_KEY"), "test", s.URL)
+	val, err := c.GetJobs(&JobRequestOptions{
+		Limit: 2,
+		Skip:  0,
+	})
+	if err == nil {
+		t.Log("Test passed, got jobs for valid user")
+	}
+
+	if len(val) == 2 {
+		t.Log("Correct amount of objects returned")
 	}
 }
 
@@ -45,8 +79,8 @@ func TestGetJobFail(t *testing.T) {
 
 	defer s.Close()
 
-	c := NewClient(os.Getenv("SAUCE_KEY"), "ssuleiman", s.URL)
-	val, err := c.GetJobs()
+	c := NewClient(os.Getenv("SAUCE_KEY"), "test", s.URL)
+	val, err := c.GetJobs(nil)
 	if val == nil && err == nil {
 		t.Log("Test passed, got an error and negative value")
 	}
