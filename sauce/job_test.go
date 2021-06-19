24752b4624541fd6206c9d77e7a8c60a3a2856a0
ssulei7 <ssuleiman955@gmail.com>
2021-06-19 01:35:33 -0500

    added positive job detail update test

--- sauce/job_test.go
+++ sauce/job_test.go
@@ -157,3 +157,64 @@ func TestGetJobDetailsFail(t *testing.T) {
 		t.Log("Got a resp back from the endpoint that was invalid")
 	}
 }
+
+func TestUpdateJob(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		update := JobDetails{}
+		json.NewDecoder(r.Body).Decode(&update)
+		resp := JobDetails{
+			BrowserShortVersion:   "90",
+			VideoURL:              update.VideoURL,
+			CreationTime:          1619124503,
+			CustomData:            nil,
+			BrowserVersion:        "90.0.4430.72",
+			Owner:                 "testuser",
+			AutomationBackend:     "webdriver",
+			ID:                    "b3d75978ab5949c9b5adb7dad4317fd0",
+			CollectsAutomatorLog:  false,
+			RecordScreenshots:     true,
+			RecordVideo:           true,
+			Build:                 nil,
+			Passed:                nil,
+			Public:                "team",
+			AssignedTunnelID:      nil,
+			Status:                "complete",
+			LogURL:                "someurl",
+			StartTime:             1619124503,
+			Proxied:               false,
+			ModificationTime:      1619124642,
+			Tags:                  nil,
+			Name:                  nil,
+			CommandsNotSuccessful: 1,
+			ConsolidatedStatus:    "error",
+			SeleniumVersion:       nil,
+			Manual:                false,
+			EndTime:               1619124642,
+			Error:                 "Test did not see a new command for 90 seconds. Timing out.",
+			Os:                    "Windows 10",
+			Breakpointed:          nil,
+			Browser:               "googlechrome",
+		}
+
+		json.NewEncoder(rw).Encode(&resp)
+	}))
+
+	defer s.Close()
+
+	c := NewClient(os.Getenv("SAUCE_KEY"), "test", s.URL)
+
+	customData := make(map[string]interface{})
+	customData["oldtest"] = true
+
+	updatedJobDetails := JobDetails{
+		VideoURL: "stuff",
+	}
+
+	val, err := c.UpdateJob("123", &updatedJobDetails)
+
+	if err == nil && val != nil {
+		t.Log("Approrpriate values returned")
+	}
+
+}
